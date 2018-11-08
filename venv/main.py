a9b587c6c72ef0a1781d2bbbd52d452d2c4322e7
HGalitska <h.galitskaya@gmail.com>
2018-11-08 14:04:25 +0200

    added some basic python

--- venv/main.py
+++ venv/main.py
@@ -2,10 +2,6 @@ import cool_module
 from importlib import reload
 
 
-def new_func(a, b):
-    return a + b
-
-
 def append_func_to_cool_module():
     file = open(r'cool_module.py', 'a')
     file.write('\n\ndef my_func(a, b):\n')
@@ -86,7 +82,43 @@ def insert_new_func_to_this_module():
     file.close()
 
 
-insert_new_func_to_this_module()
+# --------------------------------------------------------------------------
 
-# refactor_name_in_module("func", "funky")
-# insert_new_func_to_module()
+def list_cool_module_attributes():
+    print(dir(cool_module))
+
+
+def slices():
+    a = 'Python'
+    print(a[:])  # Result: Python
+    print(a[2:])  # Result: thon
+    print(a[:-4])  # Result: Py
+    print(a[2:4])  # Result: th
+    print(a[3:-1])  # Result: ho
+    print(a[::-1])  # Result: nohtyP
+    print(a[2:6:2])  # Result: to
+    print(a[2::2])  # Result: to
+
+
+def dictionaries():
+    # a = dict([[1, 'Tom'], [2, 'Stoppard']])
+    b = dict(name='Bob', surname='Johnson')
+    b['name'] = 'Kate'
+    del b['surname']
+
+    a = tuple([[1, 'Tom'], 3, 4, 'wow'])
+    print(a)
+
+
+def files():
+    file = open(r'file.txt', 'w')
+    file.writelines(['how ', 'are ', 'you?'])
+    file.flush()
+    print(file.tell())
+    file.seek(10)
+    file.writelines(['23'])
+    file.close()  # how are yo23
+
+
+# Immutable: numbers, strings, tuples, frozen sets
+# Mutable: lists, dictionaries, sets
